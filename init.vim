8a7fff820102bc84453db49ebcd523470f3f90b5
lmascelli <lmascelli@gmail.com>
2022-02-04 18:36:56 +0100

    Installing script

--- init.vim
+++ init.vim
@@ -1,4 +1,5 @@
 lua << EOF
+require 'install'
 require 'basic'
 require 'plugins'
 require 'keybinding'
---
+++ lua/install.lua
@@ -0,0 +1,35 @@
+print 'Verifying config installation'
+
+-- Creating local variables but maybe consider make them global
+
+local os_type = nil
+
+local config_path = vim.env.HOME
+local packer_path = nil
+
+if config_path == nil then
+  config_path = vim.env.XDG_HOME_CONFIG
+  os_type = 'Windows'
+  packer_path = config_path .. '/nvim-data/site/pack/'
+else
+  os_type = 'Linux'
+  packer_path = '~/.local/share/nvim/site/pack/'
+end
+
+local function install_packer()
+  os.execute('git clone --depth 1 https://www.github.com/wbthomason/packer.nvim ' .. packer_path .. 'packer/start/packer.nvim')
+end
+
+if vim.fn.isdirectory(packer_path .. 'packer') then
+  print 'Packer is installed'
+else
+  print 'Packer is not installed'
+end
+
+if os_type == 'Linux' then
+  print 'Linux Config'
+elseif os_type == 'Windows' then
+  print 'Windows Config'
+else
+  print 'Error in detecting OS type'
+end

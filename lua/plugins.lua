b3d96619e7573ce38839bda6f5cea1e8de2196b9
Retro Ginocchio <rgtwitch01@gmail.com>
2022-02-27 19:38:59 +0100

    fixing

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,11 +21,13 @@ end
 
 local packer_ok = check_packer()
 
+-- CHECK PACKER DOWNLOADED --
 if (packer_ok ~= true) then
   os.execute('git clone https://github.com/wbthomason/packer.nvim ' .. 
              packer_dir .. 'packer.nvim')
 else
-  -- INSTALL PLUGINS --
+  -- INSTALL PACKER --
+  -- DOWNLOAD PLUGINS --
   require 'packer'.startup( function()
     use 'wbthomason/packer.nvim'
     use {'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons'}
@@ -34,28 +36,31 @@ else
   )
   vim.cmd ':PackerInstall'
 
-  local plugins_downloaded = config_dir .. 'plugin_downloaded'
-  print(vim.fn.filereadable(plugins_downloaded))
-  if vim.fn.filereadable(plugins_downloaded) == 0 then
-    os.execute('echo "" >> ' .. plugins_downloaded) 
+  local packer_installed = config_dir .. 'packer_installed'
+  if vim.fn.filereadable(packer_installed) == 0 then
+    os.execute('echo "" >> ' .. packer_installed)
   else
+    local plugins_downloaded = config_dir .. 'plugin_downloaded'
+    if vim.fn.filereadable(plugins_downloaded) == 0 then
+      os.execute('echo "" >> ' .. plugins_downloaded) 
+    else
 
-  -- CONFIG PLUGINS --
-  -- bufferline --
-  vim.opt.termguicolors = true
-  require 'bufferline'.setup{}
-
-  -- toggleterm --
-  require 'toggleterm'.setup{
-    open_mapping = [[<c-\>]],
-    direction = 'float',
-    float_opts = {
-      border = 'curved'
+    -- CONFIG PLUGINS --
+    -- bufferline --
+    vim.opt.termguicolors = true
+    require 'bufferline'.setup{}
+  
+    -- toggleterm --
+    require 'toggleterm'.setup{
+      open_mapping = [[<c-\>]],
+      direction = 'float',
+      float_opts = {
+        border = 'curved'
+      }
     }
-  }
+    end
   end
 end
-
 
 return {
   packer_ok = packer_ok,

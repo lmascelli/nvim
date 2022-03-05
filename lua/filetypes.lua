97a3e006ec482caef9ae7d5dc254bec20f9b97fc
lmascelli <lmascelli@gmail.com>
2022-03-05 20:56:30 +0100

    comment macro for nasm

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,2 +1,4 @@
 -- NASM FILETYPE --
 vim.cmd 'au BufNewFile,BufRead *.nasm   setf nasm'
+local comment_macro = "o;---------------------------------\n;\n;---------------------------------ka "
+vim.fn.setreg('c', comment_macro)

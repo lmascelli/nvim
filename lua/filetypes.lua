4d321a2d941f9fcbf89450f1ff33a4a0cf8cedd6
Retro Ginocchio <rgtwitch01@gmail.com>
2022-05-29 23:16:08 +0200

    gmsh syntax

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -1,4 +1,3 @@
-
 -- NASM FILETYPE --
 
 -- set filetype = nasm for *.nasm files
@@ -12,6 +11,10 @@ vim.cmd 'au BufNewFile,BufRead *.nasm   setf nasm'
 local comment_section_macro = 'i;35a-o;\n;35a-ka '
 vim.fn.setreg('D', comment_section_macro)
 
--- insert a comment at column 37 with @C macro 
+-- insert a comment at column 37 with @C macro
 local comment_line_macro = '0$36a 36|d$a;'
 vim.fn.setreg('C', comment_line_macro)
+
+
+-- GMSH FILETYPE --
+vim.cmd 'au BufNewFile,BufRead *.geo   setf gmsh'

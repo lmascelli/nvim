8a85a88168356cdb489caf84c67313e2d3e0eaba
Retro Ginocchio <rgtwitch01@gmail.com>
2022-06-06 17:24:01 +0200

    .

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,8 @@
+--------------------------------------------------------------------------------
+--
 -- PLUGINS LIST
+--
+--------------------------------------------------------------------------------
 
 local plugins = {
   'lsp',
@@ -11,18 +15,21 @@ local plugins = {
   'nvim-telescope',
 }
 
-
+--------------------------------------------------------------------------------
+--
 -- CHECK PACKER INSTALLATION
+--
+--------------------------------------------------------------------------------
 local config_dir = ""
 local packer_dir = ""
 
-function check_packer()
+local function check_packer()
     if vim.g.win ~= nil then
         local win_dir = os.getenv("XDG_CONFIG_HOME")
         config_dir = win_dir .. "nvim/"
         packer_dir = win_dir .. "nvim-data\\site\\pack\\packer\\start\\"
     else
-        config_dir = os.getenv("HOME") .. "/.config/nvim"
+        config_dir = os.getenv("HOME") .. "/.config/nvim/"
         packer_dir = os.getenv("HOME") .. "/.local/share/nvim/site/pack/packer/start/"
     end
     if vim.fn.isdirectory(packer_dir .. 'packer.nvim') == 1 then
@@ -33,9 +40,15 @@ function check_packer()
 end
 
 local packer_ok = check_packer()
+
+--------------------------------------------------------------------------------
+--
+-- INSTALL PLUGINS
+--
+--------------------------------------------------------------------------------
+
 local can_setup = false
 
--- CHECK PACKER DOWNLOADED --
 if (packer_ok ~= true) then
     os.execute('git clone https://github.com/wbthomason/packer.nvim ' ..
         packer_dir .. 'packer.nvim')
@@ -59,6 +72,7 @@ else
         }
     end
     )
+    vim.cmd ':PackerInstall'
 
     local packer_installed = config_dir .. 'packer_installed'
     if vim.fn.filereadable(packer_installed) == 0 then
@@ -66,10 +80,8 @@ else
     else
         local plugins_downloaded = config_dir .. 'plugin_downloaded'
         if vim.fn.filereadable(plugins_downloaded) == 0 then
-            vim.cmd ':PackerInstall'
             os.execute('echo "" >> ' .. plugins_downloaded)
         else
-            local plugins_downloaded = config_dir .. 'plugin_downloaded'
             if vim.fn.filereadable(plugins_downloaded) == 0 then
                 os.execute('echo "" >> ' .. plugins_downloaded)
             else
@@ -79,12 +91,26 @@ else
     end
 end
 
-local setup = function(config)
+--------------------------------------------------------------------------------
+--
+-- CONFIGURE PLUGINS
+--
+--------------------------------------------------------------------------------
+
+
+local setup = function()
     if can_setup then
         for _, p in pairs(plugins) do
             require('p_conf.' .. p).config()
         end
     end
 end
+
+--------------------------------------------------------------------------------
+--
+-- RETURNS
+--
+--------------------------------------------------------------------------------
+
 
 return { packer_ok = packer_ok, setup = setup }
